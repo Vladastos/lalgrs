c89e1be8dc4c8a1da14da423b64357fdd8d4362c
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2025-02-26 13:07:17 +0100

    Adding dynamic types

--- src/lalgrs/structs.rs
+++ src/lalgrs/structs.rs
@@ -1,4 +1,4 @@
-use std::ops;
+use std::ops::{self, Add, Mul, Neg};
 
 use crate::lalgrs::LalgrsError;
 
@@ -7,12 +7,12 @@ use crate::lalgrs::LalgrsError;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct LalgrsVector {
-    pub values: Vec<f64>,
+pub struct LalgrsVector<T: Add<T, Output = T>> {
+    pub values: Vec<T>,
 }
 
-impl LalgrsVector {
-    pub fn new(values: Vec<f64>) -> LalgrsVector {
+impl<T: Add<T, Output = T>> LalgrsVector<T> {
+    pub fn new(values: Vec<T>) -> LalgrsVector<T> {
         LalgrsVector { values }
     }
     pub fn size(&self) -> usize {
@@ -21,12 +21,12 @@ impl LalgrsVector {
 }
 
 #[derive(Debug, Clone)]
-pub struct LalgrsMatrix {
-    columns: Vec<Vec<f64>>,
+pub struct LalgrsMatrix<T> {
+    columns: Vec<Vec<T>>,
 }
 
-impl LalgrsMatrix {
-    pub fn new(columns: Vec<Vec<f64>>) -> Result<LalgrsMatrix, LalgrsError> {
+impl<T> LalgrsMatrix<T> {
+    pub fn new(columns: Vec<Vec<T>>) -> Result<LalgrsMatrix<T>, LalgrsError> {
         let are_all_vectors_the_same_size = columns.iter().all(|v| v.len() == columns[0].len());
         if !are_all_vectors_the_same_size {
             return Err(LalgrsError::InvalidMatrixDimensions);
@@ -50,9 +50,12 @@ impl LalgrsMatrix {
 /// If one of the two vectors is empty, returns the other one.
 /// If the two vectors have different lengths and none of them are empty, returns an error.
 /// Otherwise returns a new vector which is the sum of the two operands.
-impl ops::Add<LalgrsVector> for LalgrsVector {
-    type Output = Result<LalgrsVector, LalgrsError>;
-    fn add(self, rhs: LalgrsVector) -> Self::Output {
+impl<T: Add<T, Output = T>> ops::Add<LalgrsVector<T>> for LalgrsVector<T>
+where
+    for<'a> &'a T: Add<T, Output = T>,
+{
+    type Output = Result<LalgrsVector<T>, LalgrsError>;
+    fn add(self, rhs: LalgrsVector<T>) -> Self::Output {
         if self.size() == 0 {
             return Ok(rhs);
         }
@@ -72,44 +75,64 @@ impl ops::Add<LalgrsVector> for LalgrsVector {
             self.values
                 .iter()
                 .zip(rhs.values)
-                .map(|e| -> f64 { e.0 + e.1 })
+                .map(|e| -> T { e.0 + e.1 })
                 .collect(),
         ));
     }
 }
 
 /// ## Negation of a vector. Simply multiplies each element by -1
-impl ops::Neg for LalgrsVector {
-    type Output = LalgrsVector;
+impl<T: Add<T, Output = T>> ops::Neg for LalgrsVector<T>
+where
+    for<'a> &'a T: Neg<Output = T>,
+{
+    type Output = LalgrsVector<T>;
     fn neg(self) -> Self::Output {
-        return LalgrsVector::new(self.values.iter().map(|f| -> f64 { -f }).collect());
+        return LalgrsVector::new(self.values.iter().map(|f| -> T { -f }).collect());
     }
 }
 
 /// ## Subtraction between two vectors.
 /// Performs addition with the negative of the second operand
-impl ops::Sub<LalgrsVector> for LalgrsVector {
-    type Output = Result<LalgrsVector, LalgrsError>;
-    fn sub(self, rhs: LalgrsVector) -> Self::Output {
+impl<T: Add<T, Output = T> + Neg> ops::Sub<LalgrsVector<T>> for LalgrsVector<T>
+where
+    for<'a> &'a T: Add<T, Output = T>,
+    for<'a> &'a T: Neg<Output = T>,
+{
+    type Output = Result<LalgrsVector<T>, LalgrsError>;
+    fn sub(self, rhs: LalgrsVector<T>) -> Self::Output {
         return self + (-rhs);
     }
 }
 
 /// ## Multiplication between a vector and a scalar
 /// Multiplies each element of the vector by the scalar
-impl ops::Mul<f64> for LalgrsVector {
-    type Output = LalgrsVector;
-    fn mul(self, rhs: f64) -> Self::Output {
-        LalgrsVector::new(self.values.iter().map(|v| -> f64 { v * rhs }).collect())
+impl<T: Add<T, Output = T> + Mul<T, Output = T> + Clone> ops::Mul<T> for LalgrsVector<T>
+where
+    for<'a> &'a T: Mul<T, Output = T>,
+{
+    type Output = LalgrsVector<T>;
+    fn mul(self, rhs: T) -> Self::Output {
+        LalgrsVector::new(
+            self.values
+                .iter()
+                .map(|v| -> T { v * rhs.clone() })
+                .collect(),
+        )
     }
 }
 
 /// ## Multiplication between a matrix and a vector
 /// If the number of columns in the matrix does not match the number of elements in the vector, returns an error
 /// Otherwise returns a new vector which is the result of the multiplication
-impl ops::Mul<LalgrsVector> for LalgrsMatrix {
-    type Output = Result<LalgrsVector, LalgrsError>;
-    fn mul(self, rhs: LalgrsVector) -> Self::Output {
+impl<T: Add<T, Output = T> + Clone + Mul<T, Output = T>> ops::Mul<LalgrsVector<T>>
+    for LalgrsMatrix<T>
+where
+    for<'a> &'a T: Add<T, Output = T>,
+    for<'a> &'a T: Mul<T, Output = T>,
+{
+    type Output = Result<LalgrsVector<T>, LalgrsError>;
+    fn mul(self, rhs: LalgrsVector<T>) -> Self::Output {
         if self.columns() != rhs.size() {
             return Err(LalgrsError::MismatchedVectorAndMatrixDimensions {
                 vector_size: rhs.size(),
@@ -121,13 +144,13 @@ impl ops::Mul<LalgrsVector> for LalgrsMatrix {
             .columns
             .iter()
             // Map each column to a LalgrsVector
-            .map(|col| -> LalgrsVector { return LalgrsVector::new(col.to_vec()) })
+            .map(|col| -> LalgrsVector<T> { return LalgrsVector::new(col.to_vec()) })
             // Zip each LalgrsVector with the corresponding vector element
             .zip(rhs.clone().values)
             // Multiply each LalgrsVector with the corresponding vector element
-            .map(|f| -> LalgrsVector { f.0 * f.1 })
+            .map(|f| -> LalgrsVector<T> { f.0 * f.1 })
             // Sum all LalgrsVectors
-            .reduce(|res, element| -> LalgrsVector { (res + element).unwrap() });
+            .reduce(|res, element| -> LalgrsVector<T> { (res + element).unwrap() });
 
         return Ok(result.unwrap_or(LalgrsVector::new(vec![])));
     }
@@ -136,9 +159,9 @@ impl ops::Mul<LalgrsVector> for LalgrsMatrix {
 /// ## Multiplication between two matrices
 /// If the number of columns in the first matrix does not match the number of rows in the second matrix, returns an error
 /// Otherwise returns a new matrix which is the result of the multiplication
-impl ops::Mul<LalgrsMatrix> for LalgrsMatrix {
-    type Output = Result<LalgrsMatrix, LalgrsError>;
-    fn mul(self, rhs: LalgrsMatrix) -> Self::Output {
+impl<T> ops::Mul<LalgrsMatrix<T>> for LalgrsMatrix<T> {
+    type Output = Result<LalgrsMatrix<T>, LalgrsError>;
+    fn mul(self, rhs: LalgrsMatrix<T>) -> Self::Output {
         todo!();
         return Ok(self);
     }
